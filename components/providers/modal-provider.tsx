834e045630bf3611853de610a173a90b51878e0c
Roshan Singh <roshan2152@gmail.com>
2024-01-30 12:29:20 +0530

    Edit channels

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -10,27 +10,31 @@ import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 
 export const ModalProvider = () => {
-  const [isMounted, setIsMounted] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
-  if (!isMounted) {
-    return null;
-  }
+    if (!isMounted) {
+        return null;
+    }
 
-  return (
-    <>
-      <CreateServerModal />
-      <InviteModal />
-      <EditServerModal />
-      <MembersModal />
-      <CreateChannelModal />
-      <LeaveServerModal />
-      <DeleteServerModal />
-    </>
-  )
+    return (
+        <>
+            <CreateServerModal />
+            <InviteModal />
+            <EditServerModal />
+            <MembersModal />
+            <CreateChannelModal />
+            <LeaveServerModal />
+            <DeleteServerModal />
+            <DeleteChannelModal />
+            <EditChannelModal />
+        </>
+    )
 }
